47511c2ba0d0d33cd58fb821f254025b74afb024
Nnabueze Uhiara <eze@reach.africa>
2019-04-11 14:23:20 +0100

    user can change the selection count

--- app/src/main/java/com/nezspencer/customviews/MainActivity.kt
+++ app/src/main/java/com/nezspencer/customviews/MainActivity.kt
@@ -2,11 +2,29 @@ package com.nezspencer.customviews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var dialView: DialView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.custom_fan)
+        dialView = findViewById(R.id.dialView)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            val i = it.order
+            dialView.setSelectionCount(i)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
